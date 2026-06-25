1b96acb6450dcc0b6e2562223d52c50f3b3fd8f1
Mayank Kumar <mayankcom8@gmail.com>
2026-06-25 09:04:24 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,10 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "shayad": "https://youtu.be/MJyKN-8UncM?si=gqHJzvh6k9X8Wc9r",
+    "perfect": "https://youtu.be/2Vv-BfVoq4g?si=YkOsPSnxFqPUI_0g",
+    "story": "https://youtu.be/KrsqPE9SMxo?si=KQ44ZgX5h8bnxEF1"
+
+}
